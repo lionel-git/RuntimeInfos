5e32514df008bff78be04a6e601b3e808d5d0f6d
lionel@lionel-pc2 <lionel.desorme@free.fr>
2020-05-07 17:55:37 +0200

    Update

--- RuntimeInfos/main.cpp
+++ RuntimeInfos/main.cpp
@@ -5,9 +5,15 @@ extern int tester2(void);
 
 int main(int argc, char** argv)
 {
-	std::cout << "Type key" << std::endl;
+#ifdef _DLL
+	std::cout << "/MD used" << std::endl;
+#else
+	std::cout << "/MT used" << std::endl;
+#endif
+
 	//tester();
 	tester2();
-	//int c = getchar();
 	std::cout << "Fin" << std::endl;
+	std::cout << "Type key" << std::endl;
+	int c = getchar();
 }
